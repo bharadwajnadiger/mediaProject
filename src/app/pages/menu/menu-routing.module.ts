231f383e275ce7e82dc6bdc32555c44fb697c335
bharadwajnadiger <nadigerbharadwaj@gmail.com>
2020-06-06 13:43:00 +0530

    toolbar changes done

--- src/app/pages/menu/menu-routing.module.ts
+++ src/app/pages/menu/menu-routing.module.ts
@@ -14,7 +14,12 @@ const routes: Routes = [
     {
         path: 'users',
         component:UsersComponent,
-        children : [{
+        children : [ {
+            path: '',
+            redirectTo: 'showvideo',
+            pathMatch: 'full'
+        },
+        {
             path: 'showvideo',
             component:ShowvideoComponent,
         }]
